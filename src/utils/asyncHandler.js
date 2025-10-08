a2e405c0f0920e81d6ca8243c9e7e3f434caa0e1
Suraj Kumar <surajkumar620103@gmail.com>
2025-10-08 18:55:41 +0530

    Add user registration route and fix ApiError stack variable

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,12 +1,14 @@
 
 const asyncHandler = (requestHandler) => {
-    (req, res, next) => {
+    return (req, res, next) => {
         Promise.resolve(requestHandler(req, res, next))
             .catch((err) => next(err))
     }
 }
 
 export { asyncHandler }
+
+
 
 
 
